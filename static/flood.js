5cd96f219ccc2cc1278574388bdece64e5137a9d
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-11-19 09:13:20 -0500

    Update flood.js

--- static/flood.js
+++ static/flood.js
@@ -1,9 +1,9 @@
 function flood(pin, name, num) {
-  location.href ="/" + btoa(btoa(pin)) + "/" + btoa(btoa(name)) + "/" + btoa(btoa(num)) + "/" + Math.floor(Math.random() * 999999999999999999999999) + "/flooder.html";
+  location.href = location.origin + "/" + btoa(btoa(pin)) + "/" + btoa(btoa(name)) + "/" + btoa(btoa(num)) + "/" + Math.floor(Math.random() * 999999999999999999999999) + "/flooder.html";
 }
 function kill(pin) {
-  location.href = "/die/" + btoa(btoa(pin)) + "/flooder.html";
+  location.href = location.origin + "/die/" + btoa(btoa(pin)) + "/flooder.html";
 }
 function rickroll(pin) {
-  location.href = "/lol/" + btoa(btoa(pin)) + "/rickroll.html";
+  location.href = location.origin + "/lol/" + btoa(btoa(pin)) + "/rickroll.html";
 }
